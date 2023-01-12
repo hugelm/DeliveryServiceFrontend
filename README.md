7b0c715afd08a15ee88e195d1593b480ca90d714
hugelm <mshgl@gmx.de>
2023-01-12 16:02:17 +0100

    added myorders

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ sudo docker build -t lieferservice-deliveryservicefrontend .
 run image:\
 sudo docker run --name LieferserviceFE -d -p 80:80 lieferservice-deliveryservicefrontend
 
-pull image from docker hub (may outdated verison):\
+pull image from docker hub:\
 sudo docker run --name LieferserviceFE -d -p 80:80 hugelm/lieferservice-deliveryservicefrontend:latest
---
+++ js/myorder.js
@@ -0,0 +1,55 @@
+function getOrders(){
+
+    let name = document.getElementById("name").value
+
+    putDataInTable();
+  
+    function IncDec() {
+      decs = document.querySelectorAll(".dec");
+      incs = document.querySelectorAll(".inc");
+      decs.forEach((dec) => {
+        dec.addEventListener("click",function(e){
+          if(e.target.nextElementSibling.value > 0){
+            e.target.nextElementSibling.value--;
+            e.target.nextElementSibling.innerHTML = e.target.nextElementSibling.value;
+          }
+        })
+      })
+      incs.forEach((inc) => {
+        inc.addEventListener("click",function(e){
+          e.target.previousElementSibling.value++;
+          e.target.previousElementSibling.innerHTML = e.target.previousElementSibling.value;
+        })
+      })
+    }
+  
+    async function getPromiseOfAllProducts() {
+          console.log('http://localhost:8080/delivery?name='+name);
+          return fetch('http://localhost:8080/delivery?name='+name, {
+          method: 'GET',
+        })
+          .then(function(response) {
+            return response.json();
+          })
+    }
+  
+    async function putDataInTable() {
+          data = await getPromiseOfAllProducts();
+          var tr;
+          $.each(data, function(k, record) {
+            tr = $("<tr></tr>");
+            tr.append("<td>" + record.id + "</td>");
+            tr.append("<td>" + record.name + "</td>");
+            tr.append("<td>" + record.items + "</td>");
+            tr.append("<td>" + record.address["postcode"] + ", " + record.address["street"] + " " + record.address["housenumber"] + "</td>");
+            tr.append("<td>" + record.items + "</td>");
+            tr.append("<td>" + record.price +"€" + "</td>");
+            tr.append("<td>" +
+            "<button type='button' onclick='deleteOrder(this.id)' class='btn btn-warning' id=" + record.id + ">als ausgeliefert markieren</button>" +
+            "</div></td>");
+            $("#productsTable").append(tr);
+          });
+          IncDec();
+      }
+      
+    };
